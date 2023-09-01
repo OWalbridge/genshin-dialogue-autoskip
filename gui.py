4af2e4e3f421855deaa8af8463e6f9ef1f6f826a
Owen <141431038+OWalbridge@users.noreply.github.com>
2023-09-01 23:26:43 +1000

    UI now uses config file. Updated git ignore. Updated customisation options. Organised GUI.
    UI can now load from config file a config file and make one if it doesn't exist. Added this file to GitIgnore. Added further customisation options. Reorganised the GUI and added a how to use to the in app read me.

--- gui.py
+++ gui.py
@@ -9,7 +9,6 @@
 
 #TODOs (More info in comments at relevent parts of code):
 #
-# Add readme from a file as opposed to hard coded
 # Append time to start of console output
 # Add more console error catching (if needed)
 
@@ -30,26 +29,47 @@ except ImportError: # If not on Windows, use dummy function to return 1920x1080
         elif metric == 1:
             return 1080
 
+# Global Variables -------------------------------------------------------------
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
+# These need to be global to load them into the UI and create init config file
+# as opposed to class attributes
+SCREEN_WIDTH = GetSystemMetrics(0)
+SCREEN_HEIGHT = GetSystemMetrics(1)
+
+# Config -----------------------------------------------------------------------
+
+try: # Check for existing config
+    file = open("config.txt", "r")
+    file.close()
+except: # If no config, create default one
+    file = open("config.txt", "w")
+    file.write(str(SCREEN_WIDTH) + "|" + str(SCREEN_HEIGHT) + "|Keyboard|System|#1f6aa5|#134870|White|100%")
+    file.close()
+
+# Load themes from config
+file = open("config.txt", "r")
+config = file.read().split("|")
+butt_theme = config[4]
+butt_hover_theme = config[5]
+text_theme = config[6]
+file.close()
+
 class SkipperGUI(customtkinter.CTk):
     # Class attributes ---------------------------------------------------------
-
-    #Screen Resolution
-    # Collecting this information here, as opposed to another file, as users
-    # can input their own resolution.
-    SCREEN_WIDTH = GetSystemMetrics(0)
-    SCREEN_HEIGHT = GetSystemMetrics(1)
 
     # GUI ----------------------------------------------------------------------
     def __init__(self):
         super().__init__()
 
+        global SCREEN_WIDTH
+        global SCREEN_HEIGHT
+
         # Configure window
         self.title("Genshin Impact Dialogue Skip")
         self.deiconify()
-        self.geometry(f"{380}x{340}")
+        self.geometry(f"{380}x{430}")
         self.resizable(False, False)
 
         # Set grid layout 1x2
@@ -96,33 +116,40 @@ class SkipperGUI(customtkinter.CTk):
         self.configure_frame.grid_columnconfigure(0, weight=0)
 
         # Resolution
-        self.configure_resolution_label = customtkinter.CTkLabel(self.configure_frame, text="Current Resolution:\n" + str(self.SCREEN_WIDTH) + 'x' + str(self.SCREEN_HEIGHT), anchor="w")
-        self.configure_resolution_label.grid(row=1, column=0, padx=30, pady=(10, 10))
+        self.configure_resolution_label = customtkinter.CTkLabel(self.configure_frame, text="Current Resolution:\n" + str(SCREEN_WIDTH) + 'x' + str(SCREEN_HEIGHT), anchor="w")
+        self.configure_resolution_label.grid(row=1, column=0, padx=30, pady=(15, 0))
 
         self.configure_resolution_entry_width = customtkinter.CTkEntry(self.configure_frame, placeholder_text="Enter custom width")
         self.configure_resolution_entry_width.grid(row=2, column=0, padx=30, pady=(10, 10))
         self.configure_resolution_entry_height = customtkinter.CTkEntry(self.configure_frame, placeholder_text="Enter custom height")
         self.configure_resolution_entry_height.grid(row=3, column=0, padx=30, pady=(10, 10))
 
-        self.configure_resolution_button = customtkinter.CTkButton(self.configure_frame,command = self.update_resolution_button_event, text="Update Resolution")
+        self.configure_resolution_button = customtkinter.CTkButton(self.configure_frame,command = self.update_resolution_button_event, text="Update Resolution", fg_color=butt_theme, hover_color=butt_hover_theme, text_color=text_theme )
         self.configure_resolution_button.grid(row=4, column=0, padx=20, pady=(10, 10)) 
 
         # Configure Input Method
         self.configure_input_method_label = customtkinter.CTkLabel(self.configure_frame, text="Input Method:", anchor="w")
         self.configure_input_method_label.grid(row=5, column=0, padx=30, pady=(10, 0))
         self.configure_input_method_optionemenu = customtkinter.CTkOptionMenu(self.configure_frame, values=["Keyboard"],
-                                                                            command=self.change_input_type_event)
+                                                                            command=self.change_input_type_event, fg_color=butt_theme, text_color=text_theme)
         self.configure_input_method_optionemenu.grid(row=6, column=0, padx=30, pady=(10, 10))
 
-        self.configure_input_method_comingsoon_label = customtkinter.CTkLabel(self.configure_frame, text="Remember to enable\nautoskip!", anchor="w")
-        self.configure_input_method_comingsoon_label.grid(row=7, column=0, padx=30, pady=(10, 0))
+        # Save Button
+        self.configure_save_label = customtkinter.CTkLabel(self.configure_frame, text="Save config:", anchor="w")
+        self.configure_save_label.grid(row=7, column=0, padx=30, pady=(10, 0))
+        self.configure_save_button = customtkinter.CTkButton(self.configure_frame, text="Save config", fg_color=butt_theme, hover_color=butt_hover_theme, text_color=text_theme)
+        self.configure_save_button.grid(row=8, column=0, padx=20, pady=(10, 10))
+
+        # Notice
+        self.configure_input_method_label = customtkinter.CTkLabel(self.configure_frame, text="Remember to enable\nautoskip!", anchor="w")
+        self.configure_input_method_label.grid(row=9, column=0, padx=30, pady=(10, 10))
 
         # Readme frame ---------------------------------------------------------
         self.readme_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.readme_frame.grid_columnconfigure(0, weight=0)
 
         # Textbox
-        self.readme_frame_txtbox = customtkinter.CTkTextbox(master = self.readme_frame, corner_radius=0,height = 340, state = "disabled", wrap = "word")
+        self.readme_frame_txtbox = customtkinter.CTkTextbox(master = self.readme_frame, corner_radius=0,height = 430, state = "disabled", wrap = "word")
         self.readme_frame_txtbox.grid(row=0, column=0)
         self.readme_frame_txtbox.grid(row=0, column=0, sticky="nsew")
 
@@ -131,12 +158,12 @@ class SkipperGUI(customtkinter.CTk):
         self.console_frame.grid_columnconfigure(0, weight=0)
 
         # Textbox
-        self.console_frame_txtbox = customtkinter.CTkTextbox(master = self.console_frame, corner_radius=0,height = 291, state = "disabled", wrap = "word")
+        self.console_frame_txtbox = customtkinter.CTkTextbox(master = self.console_frame, corner_radius=0,height = 380, state = "disabled", wrap = "word")
         self.console_frame_txtbox.grid(row=0, column=0)
         self.console_frame_txtbox.grid(row=0, column=0, sticky="nsew")
 
         # Export Button
-        self.console_frame_export_button = customtkinter.CTkButton(self.console_frame, command=self.export_button_event, text="Export")
+        self.console_frame_export_button = customtkinter.CTkButton(self.console_frame, command=self.export_button_event, text="Export", fg_color=butt_theme, hover_color=butt_hover_theme, text_color=text_theme)
         self.console_frame_export_button.grid(row=1, column=0, padx=20, pady=10)
 
         # Customise frame ------------------------------------------------------
@@ -144,35 +171,69 @@ class SkipperGUI(customtkinter.CTk):
         self.customise_frame.grid_columnconfigure(0, weight=0)
         # Appearance Mode
         self.appearance_mode_label = customtkinter.CTkLabel(self.customise_frame, text="Appearance:", anchor="w")
-        self.appearance_mode_label.grid(row=1, column=0, padx=30, pady=(20, 0))
+        self.appearance_mode_label.grid(row=1, column=0, padx=30, pady=(15, 0))
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.customise_frame, values=["Light", "Dark", "System"],
-                                                                       command=self.change_appearance_mode_event)
+                                                                       command=self.change_appearance_mode_event, fg_color=butt_theme, text_color=text_theme)
         self.appearance_mode_optionemenu.grid(row=2, column=0, padx=30, pady=(10, 10))
         # Theme
-        self.custom_theme_label = customtkinter.CTkLabel(self.customise_frame, text="Custom Theme:", anchor="e")
+        self.custom_theme_label = customtkinter.CTkLabel(self.customise_frame, text="Custom Theme:\n(updates on restart)", anchor="e")
         self.custom_theme_label.grid(row=3, column=0, padx=30, pady=(10, 0))
-        self.custom_theme_entry = customtkinter.CTkEntry(self.customise_frame, placeholder_text="Enter hex code")
-        self.custom_theme_entry.grid(row=4, column=0, padx=30, pady=(10, 10))
-        self.custom_theme_update_button = customtkinter.CTkButton(self.customise_frame, text="Update theme", command=self.change_theme_event)
-        self.custom_theme_update_button.grid(row=5, column=0, padx=20, pady=(10, 10))
+
+        self.custom_theme_entry_fg_color = customtkinter.CTkEntry(self.customise_frame, placeholder_text="Button HEX")
+        self.custom_theme_entry_fg_color.grid(row=4, column=0, padx=30, pady=(10, 10))
+
+        self.custom_theme_entry_hover_color = customtkinter.CTkEntry(self.customise_frame, placeholder_text="Button hover HEX")
+        self.custom_theme_entry_hover_color.grid(row=5, column=0, padx=30, pady=(10, 10))
+
+        self.custom_theme_text_optionmenu = customtkinter.CTkOptionMenu(self.customise_frame, values=["White text", "Black text"],
+                                                                          command=self.change_theme_event, fg_color=butt_theme, text_color=text_theme)
+        self.custom_theme_text_optionmenu.grid(row=6, column=0, padx=30, pady=(10, 10))
+
+        self.custom_theme_update_button = customtkinter.CTkButton(self.customise_frame, text="Update theme", command=self.change_theme_event, fg_color=butt_theme, hover_color=butt_hover_theme, text_color=text_theme)
+        self.custom_theme_update_button.grid(row=7, column=0, padx=20, pady=(10, 10))
 
         # Scaling
         self.scaling_label = customtkinter.CTkLabel(self.customise_frame, text="Scale:", anchor="w")
-        self.scaling_label.grid(row=7, column=0, padx=30, pady=(10, 0))
+        self.scaling_label.grid(row=8, column=0, padx=30, pady=(10, 0))
         self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.customise_frame, values=["80%", "90%", "100%", "110%", "120%"],
-                                                               command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=8, column=0, padx=30, pady=(10, 20))
+                                                               command=self.change_scaling_event, fg_color=butt_theme, text_color=text_theme)
+        self.scaling_optionemenu.grid(row=9, column=0, padx=30, pady=(10, 20))
 
         # Set default values ---------------------------------------------------
         self.stop_button.configure(state="disabled", fg_color=('darkred'))
-        self.appearance_mode_optionemenu.set("System")
-        self.scaling_optionemenu.set("100%")
-        self.select_frame_by_name("configure")
         self.configure_input_method_optionemenu.set("Keyboard")
+        self.select_frame_by_name("configure")
 
-        # TODO: Add readme from a file as opposed to hard coded; this is temp
+        # Load custom config.txt
+        try:
+            file = open("config.txt", "r")
+            config = file.read().split("|")
+
+            # Set appearance mode
+            self.appearance_mode_optionemenu.set(config[3]) 
+            customtkinter.set_appearance_mode(config[3])
+
+            # Set scaling
+            self.scaling_optionemenu.set(config[7])
+            new_scaling_float = int(config[7].replace("%", "")) / 100
+            customtkinter.set_widget_scaling(new_scaling_float)
+            customtkinter.set_window_scaling(new_scaling_float)
+
+            # Set resolution
+            SCREEN_WIDTH = int(config[0])
+            SCREEN_HEIGHT = int(config[1])
+            self.configure_resolution_label.configure(text="Current Resolution:\n" + str(SCREEN_WIDTH) + 'x' + str(SCREEN_HEIGHT))
+
+            # Set input method
+            self.configure_input_method_optionemenu.set(config[2])
+
+
+        except:
+            self.appearance_mode_optionemenu.set("System")
+            self.scaling_optionemenu.set("100%")
+        
         self.readme_frame_txtbox.configure(state='normal') # enable editing
-        self.readme_frame_txtbox.insert("0.0", "WARNING: There is a possibility that you get banned for using this program.\nUse at your own risk!\n\nThis program detects pixel colors to determine when to skip dialogue. It can misfire so disable when not in dialogue.\n\nThis project is under the MIT License.\n\n Contributors:\n-Hubert Rozmarynowski\n(Git Management, Pixel Coordiantes, Dialogue Selection, Documentation)\n-Owen Walbridge\n(UI, All Res Support, Fixes, Documentation)\n-xdenotte\n(Xbox Support)\n-Vamqueror\n(Dialog Selection, Fixes)\n-vlsido\n(DS4 Support)\n-YotamZiv298\n(Documentation and Code Clarity)")
+        self.readme_frame_txtbox.insert("0.0", "WARNING: There is a possibility you get banned for using this program. Use at your own risk!\n\nHow to use:\nEnsure the \"current resolution\" and \"input method\" are correct and hit \"start\". Head back to Genshin and dialoge will be auto clicked.\n\nThis program detects pixel colors to determine when to skip dialogue. It can misfire; disable when not in dialogue.\n\nThis project is under the MIT License.\n\n Contributors:\n-Hubert Rozmarynowski\n(Git Management, Pixel Coordiantes, Dialogue Selection, Documentation)\n\n-Owen Walbridge\n(UI, All Res Support, Fixes, Documentation)\n\n-xdenotte\n(Xbox Support)\n\n-Vamqueror\n(Dialog Selection, Fixes)\n\n-vlsido\n(DS4 Support)\n\n-YotamZiv298\n(Documentation and Code Clarity)")
         self.readme_frame_txtbox.configure(state='disabled') # disable editing
         
     
@@ -269,14 +330,16 @@ class SkipperGUI(customtkinter.CTk):
         height_entry = self.configure_resolution_entry_height.get()
         if width_entry and height_entry:  # Check if both entries are not empty
             try:
-                self.SCREEN_WIDTH = int(width_entry)
-                self.SCREEN_HEIGHT = int(height_entry)
-                print("Resolution updated to " + str(self.SCREEN_WIDTH) + 'x' + str(self.SCREEN_HEIGHT))
+                global SCREEN_WIDTH
+                global SCREEN_HEIGHT
+                SCREEN_WIDTH = int(width_entry)
+                SCREEN_HEIGHT = int(height_entry)
+                print("Resolution updated to " + str(SCREEN_WIDTH) + 'x' + str(SCREEN_HEIGHT))
             except ValueError:
                 print("Invalid input. Please enter valid integers for width and height.")
         else:
             print("Please enter values for both width and height.")
-        self.configure_resolution_label.configure(text="Current Resolution:\n" + str(self.SCREEN_WIDTH) + 'x' + str(self.SCREEN_HEIGHT))
+        self.configure_resolution_label.configure(text="Current Resolution:\n" + str(SCREEN_WIDTH) + 'x' + str(SCREEN_HEIGHT))
 
     def change_input_type_event(self, new_input_type: str):
         print("Input type changed to " + new_input_type)
@@ -304,7 +367,13 @@ class SkipperGUI(customtkinter.CTk):
     
     def change_theme_event(self):
         #print("Theme button clicked")
-        pass
+        try: 
+            theme = self.custom_theme_entry.get()
+            print("Theme changed to " + theme)
+        except:
+            print("Invalid Input. Please enter a valid hex code.")
+        else:
+            print("Please enter a hex value")
 
     def change_scaling_event(elf, new_scaling: str):
         print("Scaling changed to " + new_scaling)
